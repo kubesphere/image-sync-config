f8b928d4414ddc9f44946c531eae1dfe436ceace
Roland.Ma <rolandma@yunify.com>
2020-12-11 07:22:00 +0000

    refine pipeline

--- .github/workflows/sync.sh
+++ .github/workflows/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Only need to sync images that changed in current commit
-FILES=$(git diff-tree --no-commit-id --name-only -r $COMMITID)
+FILES=$(git diff-tree --no-commit-id --name-only -r $COMMITID |grep rules)
 
 for FILE in $FILES
 do
