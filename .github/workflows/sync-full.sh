834958eef73538a940c77233d9173e41979bc70d
Jeff <jeffzhang@yunify.com>
2021-04-27 09:42:40 +0000

    add v3.1.0 images
    Signed-off-by: Jeff <jeffzhang@yunify.com>

--- .github/workflows/sync-full.sh
+++ .github/workflows/sync-full.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Only need to sync images that changed in current commit
-FILES=$(ls rules)
+FILES=$(find rules -type f)
 
 for FILE in $FILES
 do
-     echo "$FILES"
-    ./image-syncer --auth=./auth.json --images=rules/$FILE
+     echo "========> Syncing: $FILES <========"
+    ./image-syncer --auth=./auth.json --images=${FILE}
 done
